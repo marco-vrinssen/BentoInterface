23c7f711f21049c02f1e7aecf3ace44e6983a7fb
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2024-06-03 11:15:09 +0200

    Add dialog close functions

--- items.lua
+++ items.lua
@@ -55,3 +55,40 @@ end
 local MerchantEvents = CreateFrame("Frame")
 MerchantEvents:RegisterEvent("MERCHANT_SHOW")
 MerchantEvents:SetScript("OnEvent", AutoSellRepair)
+
+
+
+
+DialogEvents:SetScript("OnEvent", function(self, event, ...)
+    if event == "CONFIRM_LOOT_ROLL" then
+        local id, rollType = ...
+        ConfirmLootRoll(id, rollType)
+    elseif event == "CONFIRM_DISENCHANT_ROLL" then
+        local id, rollType = ...
+        ConfirmLootRoll(id, rollType)
+    elseif event == "LOOT_BIND_CONFIRM" then
+        local slot = ...
+        ConfirmLootSlot(slot)
+    elseif event == "EQUIP_BIND_CONFIRM" then
+        local slot = ...
+        EquipPendingItem(slot)
+    elseif event == "MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL" then
+        local slot = ...
+        SellCursorItem()
+    elseif event == "DELETE_ITEM_CONFIRM" then
+        StaticPopupDialogs["DELETE_ITEM"].OnAccept()
+    end
+end)
+
+StaticPopupDialogs["DELETE_ITEM"].OnShow = function(self)
+    self.editBox:SetText(DELETE_ITEM_CONFIRM_STRING)
+end
+
+
+local DialogEvents = CreateFrame("Frame")
+DialogEvents:RegisterEvent("CONFIRM_LOOT_ROLL")
+DialogEvents:RegisterEvent("CONFIRM_DISENCHANT_ROLL")
+DialogEvents:RegisterEvent("LOOT_BIND_CONFIRM")
+DialogEvents:RegisterEvent("EQUIP_BIND_CONFIRM")
+DialogEvents:RegisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
+DialogEvents:RegisterEvent("DELETE_ITEM_CONFIRM")
