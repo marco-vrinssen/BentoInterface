867131ad5d7aa74e4ab0c762f10855933a80e66a
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2024-06-06 17:03:29 +0200

    Refactor Dialogs.lua to simplify event handling and function calls

--- Modules/Dialogs.lua
+++ Modules/Dialogs.lua
@@ -1,23 +1,13 @@
-local function DialogUpdate()
+local function DialogUpdate(_, event, ...)
     if event == "CONFIRM_LOOT_ROLL" then
-        local id, rollType = ...
-        ConfirmLootRoll(id, rollType)
+        ConfirmLootRoll(...)
     elseif event == "LOOT_BIND_CONFIRM" then
-        local slot = ...
-        ConfirmLootSlot(slot)
+        ConfirmLootSlot(...)
     elseif event == "EQUIP_BIND_CONFIRM" then
-        local slot = ...
-        EquipPendingItem(slot)
+        EquipPendingItem(...)
     elseif event == "MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL" then
-        local slot = ...
         SellCursorItem()
-    elseif event == "DELETE_ITEM_CONFIRM" then
-        StaticPopupDialogs["DELETE_ITEM"].OnAccept()
     end
-end)
-
-StaticPopupDialogs["DELETE_ITEM"].OnShow = function(self)
-    self.editBox:SetText(DELETE_ITEM_CONFIRM_STRING)
 end
 
 local DialogEvents = CreateFrame("Frame")
@@ -25,5 +15,4 @@ DialogEvents:RegisterEvent("CONFIRM_LOOT_ROLL")
 DialogEvents:RegisterEvent("LOOT_BIND_CONFIRM")
 DialogEvents:RegisterEvent("EQUIP_BIND_CONFIRM")
 DialogEvents:RegisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
-DialogEvents:RegisterEvent("DELETE_ITEM_CONFIRM")
 DialogEvents:SetScript("OnEvent", DialogUpdate)
