a4f3ce2b77f1ea47daf010c33a16788a05d98f08
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2024-06-19 15:02:58 +0200

    Restructure code and update modules

--- Modules/Items.lua
+++ Modules/Items.lua
@@ -1,3 +1,18 @@
+local function AutoLootUpdate()
+    if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+        for i = GetNumLootItems(), 1, -1 do
+            LootSlot(i)
+        end
+    end
+end
+
+local LootEvents = CreateFrame("Frame")
+LootEvents:RegisterEvent("LOOT_READY")
+LootEvents:SetScript("OnEvent", AutoLootUpdate)
+
+
+
+
 local function AutoSellRepair()
     local RepairCost, TotalSold = 0, 0
     local CanMerchantRepair, RepairAllItems, GetRepairAllCost = CanMerchantRepair, RepairAllItems, GetRepairAllCost
