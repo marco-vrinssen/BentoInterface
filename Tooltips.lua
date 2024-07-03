90fbf59ffffded1109088dbbd5c5d30f66daa988
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2024-07-03 13:24:40 +0200

    Adjust tooltip statusbar for units

--- Tooltips.lua
+++ Tooltips.lua
@@ -1,5 +1,5 @@
 local function UpdateStatusBarSize(self)
-    GameTooltipStatusBar:SetSize(self:GetWidth() - 8, 10)
+    GameTooltipStatusBar:SetSize(self:GetWidth() - 4, 12)
 end
 
 local function UnitTooltipUpdate(GameTooltip)
@@ -8,7 +8,7 @@ local function UnitTooltipUpdate(GameTooltip)
         GameTooltip:SetPoint("TOPLEFT", BentoUI.TargetPortraitBackdrop, "BOTTOMRIGHT", 0, 0)
 
         GameTooltipStatusBar:ClearAllPoints()
-        GameTooltipStatusBar:SetPoint("TOP", GameTooltip, "BOTTOM", 0, 2)
+        GameTooltipStatusBar:SetPoint("TOP", GameTooltip, "BOTTOM", 0, 4)
         GameTooltipStatusBar:SetStatusBarTexture("Interface/RaidFrame/Raid-Bar-HP-Fill.blp")
         GameTooltipStatusBar:SetFrameLevel(GameTooltip:GetFrameLevel() -1)
     end
